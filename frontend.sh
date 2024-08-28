1cf9333071777809bd4e7e2bbebf023fc34d15eb
Narahari <narahari517@gmail.com>
2024-08-28 20:54:38 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -15,7 +15,7 @@ N="\e[0m"
 root_check(){
     if [ $userid -ne 0 ]
     then
-        echo -e "$R Please run the script with root previleges $N" | tee -a $logfile
+        echo -e "$R Please run the script with root previleges $N" | tee -a $Logfile
         exit 1
     fi
 }
@@ -23,35 +23,35 @@ root_check(){
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" | tee -a $logfile
+        echo -e "$2 is...$R FAILED $N" | tee -a $Logfile
     else
-        echo -e "$2 is...$G SUCCESS $N" | tee -a $logfile
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $Logfile
     fi
 }
 
-echo "Script started running at: $(date)" | tee -a $logfile
+echo "Script started running at: $(date)" | tee -a $Logfile
 
 root_check
 
-dnf install nginx -y &>>$logfile
+dnf install nginx -y &>>$Logfile
 validate $? "Installing nginx"
 
-systemctl enable nginx &>>$logfile
+systemctl enable nginx &>>$Logfile
 validate $? "Enabling nginx"
 
-systemctl start nginx &>>$logfile
+systemctl start nginx &>>$Logfile
 validate $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$logfile
+rm -rf /usr/share/nginx/html/* &>>$Logfile
 validate $? "Removing default website"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$logfile
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$Logfile
 validate $? "Downloading frontend code"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$logfile
+unzip /tmp/frontend.zip &>>$Logfile
 validate $? "Extracting frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$logfile
-validate $? "copied expense conf"
-systemctl restart nginx 
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$Logfile
+validate $? "copied expense.conf"
+systemctl restart nginx
